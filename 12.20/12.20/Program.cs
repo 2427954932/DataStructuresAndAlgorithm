1e0533aff20603d52bf37732004dbb0ea975c189
ZZ-0509 <1016046732@qq.com>
2019-12-20 10:06:22 +0800

    12.20

--- 12.20/12.20/Program.cs
+++ 12.20/12.20/Program.cs
@@ -6,7 +6,31 @@ namespace _12._20
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string str = Console.ReadLine();
+            int j = 0;
+            int a = 0;
+            string[,] strl =
+            {
+                {"张三","2","3","张三" },
+                {"张三","2","3","张三" },
+                {"张三","2","3","张三" }
+            };
+            foreach (string i in strl)
+            {
+                if (j == 2)
+                {
+                    j = 0;
+                    a++;
+                }
+                if (i == str)
+                {
+                    Console.WriteLine("坐标在" + j + a);
+                }
+                j++;
+
+                continue;
+            }
+            Console.ReadLine();
         }
     }
 }
